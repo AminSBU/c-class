94ea7466c312bbe2079467018b68b414d2fd187e
DESKTOP-HSJEIOG\monsieur.A <omid.elc88@gmail.com>
2024-03-28 01:12:09 +0330

    02_c_in_out_p2.c

---
+++ 01_c_input_output/02_c_in_out_p2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Driver code
+int main()
+{
+    float val = 12.3;
+
+    printf("Using %%f: %f\n", val); // show float type
+    printf("Using %%e: %e\n", val); // show scientific notation of floats
+    printf("Using %%E: %E\n", val); // show scientific notation of floats
+    return 0;
+}
