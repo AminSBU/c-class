258f8aba099a49bd35ddcb30657666ff849ffc4f
DESKTOP-HSJEIOG\monsieur.A <omid.elc88@gmail.com>
2024-03-28 01:29:35 +0330

    03_c_in_out_p3.c

---
+++ 01_c_input_output/03_c_in_out_p3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Driver code
+int main()
+{
+    int value;
+
+    printf("enter number in decimal: ");
+    scanf("%d", &value);
+
+    printf("\n your number in hexadecimal is: %X\n", value);
+    return 0;
+}
